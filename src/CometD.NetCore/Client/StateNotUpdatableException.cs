ccbca0812ef605987b0a62040e5962b74f095fb0
stephan.klauberg <stephan.klauberg@mailbox.org>
2023-11-24 14:07:47 +0100

    force reconnection

---
+++ src/CometD.NetCore/Client/StateNotUpdatableException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CometD.NetCore.Client
+{
+    [Serializable]
+    internal class StateNotUpdatableException : Exception
+    {
+        public StateNotUpdatableException()
+        {
+        }
+
+        public StateNotUpdatableException(string message) : base(message)
+        {
+        }
+
+        public StateNotUpdatableException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected StateNotUpdatableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
